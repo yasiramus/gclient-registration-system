a56d2460277c3e8597ee8113c1b2f366032da46e
yasiramus <yasirayusif@gmail.com>
2025-08-07 18:58:12 +0000

    protected report routes

--- backend/src/routes/report.routes.ts
+++ backend/src/routes/report.routes.ts
@@ -7,6 +7,12 @@ import {
   getTotalIncome,
   getTotalLearners,
 } from "../controllers/report.controller";
+import { authorizeRoles, requireAuth } from "../middleware/auth.middleware";
+import { Role } from "../../generated/prisma";
+
+// Group Protected Routes
+reportRouter.use(requireAuth);
+reportRouter.use(authorizeRoles(Role.SUPER_ADMIN || Role.ADMIN));
 
 reportRouter.get("/total-learners", getTotalLearners);
 
